7ed86b0fdb89358aec8e8af0fb219c2d7d0bbf8b
Sameerbasha <sameer26basha@gmail.com>
2023-04-14 17:40:32 +0530

    Added initial logic to star shows with useReducer, Created usePersistedReducer to sync with local storage

--- src/components/shows/ShowsCard.jsx
+++ src/components/shows/ShowsCard.jsx
@@ -1,4 +1,4 @@
-const ShowsCard = ({ name, image, id, summary }) => {
+const ShowsCard = ({ name, image, id, summary, onStarMeClick }) => {
   const summaryStriped = summary
     ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
     : 'No Description';
@@ -15,7 +15,9 @@ const ShowsCard = ({ name, image, id, summary }) => {
         <a href={`/show/${id}`} target="_blank" rel="noreferrer">
           Read More
         </a>
-        <button type="button">Star Me</button>
+        <button type="button" onClick={() => onStarMeClick(id)}>
+          Star Me
+        </button>
       </div>
     </div>
   );
--- src/components/shows/ShowsGrid.jsx
+++ src/components/shows/ShowsGrid.jsx
@@ -1,6 +1,47 @@
 import ShowsCard from './ShowsCard';
+import { useReducer, useEffect } from 'react';
+
+const usePersistedReducer = (reducer, initialState, localStorageKey) => {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persistedValue = localStorage.getItem(localStorageKey);
+    return persistedValue ? JSON.parse(persistedValue) : initial;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, JSON.stringify(state));
+  }, [state, localStorageKey]);
+
+  return [state, dispatch];
+};
+
+const starredShowReducer = (currentStarred, action) => {
+  switch (action.type) {
+    case 'STAR':
+      return currentStarred.concat(action.showId);
+    case 'UNSTAR':
+      return currentStarred.filter(showId => showId !== action.showId);
+    default:
+      return currentStarred;
+  }
+};
 
 const ShowsGrid = ({ apiData }) => {
+  const [starredShow, dispatchStarred] = usePersistedReducer(
+    starredShowReducer,
+    [],
+    'starredShows'
+  );
+  console.log(starredShow);
+  const onStarMeClick = showId => {
+    const isStarred = starredShow.includes(showId);
+
+    if (isStarred) {
+      dispatchStarred({ type: 'UNSTAR', showId });
+    } else {
+      dispatchStarred({ type: 'STAR', showId });
+    }
+  };
+
   return apiData.map(data => (
     <ShowsCard
       key={data.show.id}
@@ -8,6 +49,7 @@ const ShowsGrid = ({ apiData }) => {
       image={data.show.image ? data.show.image.medium : '/Not-Found.png'}
       id={data.show.id}
       summary={data.show.summary}
+      onStarMeClick={onStarMeClick}
     />
   ));
 };
